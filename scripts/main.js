7aa5780abc88cc1b3c2e8d634dc6a8bedf96d283
Scotty Wyatt <crashy@Scottys-MacBook-Pro.local>
2021-05-17 16:29:39 -0400

    website created

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+function changeImage() {
+  let img = document.getElementById("image");
+  img.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw5JEljMjVLBgJaT5LuzjdqTftt3CBqkTvtw&usqp=CAU";
+  return false;
+}
